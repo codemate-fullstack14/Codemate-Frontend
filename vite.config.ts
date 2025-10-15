887fb79fbe1262b807afa025caf513be0b3b8707
sonseongyeol <sonseong10@gmail.com>
2025-10-15 14:08:25 +0900

    Fix githubpage deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // 배포용 빌드 산출물 폴더 지정
+  },
 });
