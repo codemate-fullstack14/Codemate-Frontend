912e31eba3e072c9dea6519f20f1ad46e24b9a61
sonseongyeol <sonseong10@gmail.com>
2025-10-16 13:35:16 +0900

    Apply router Suspense lazy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { lazy, Suspense } from "react";
 
 import LayoutContainer from "./components/layouts/layout";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
-// import ProtectedRoute from "./utils/ProtectedRoute";
 
 const TutorialPage = lazy(() => import("./pages/challenge/tutorial"));
 const MemberPage = lazy(() => import("./pages/member"));
@@ -18,8 +17,23 @@ function App() {
     <>
       <Routes>
         {/* 로그인 페이지는 보호하지 않음 */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/user/register" element={<UserRegister />} />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<LoadingSpinner />}>
+              <LoginPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/user/register"
+          element={
+            <Suspense fallback={<LoadingSpinner />}>
+              <UserRegister />
+            </Suspense>
+          }
+        />
+
         {/* jwt 보호 라우트 로그인 연동시 작업 해야함 */}
         {/* <Route element={<ProtectedRoute />}>
         </Route> */}
@@ -49,7 +63,26 @@ function App() {
               </Suspense>
             }
           />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<LoadingSpinner />}>
+                <div className="flex flex-col justify-center items-center min-h-[calc(100vh-60px)] px-4 text-center">
+                  <strong className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-2">
+                    404 Error
+                  </strong>
+                  <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-3">
+                    페이지를 찾을 수 없습니다.
+                  </h1>
+                  <p className="tossface text-sm sm:text-base md:text-lg text-gray-600 max-w-md">
+                    요청하신 페이지가 존재하지 않거나 이동되었을 수 있어요. 🙏
+                  </p>
+                </div>
+              </Suspense>
+            }
+          />
         </Route>
+
         {/* 알고리즘 문제를 풀 공간 */}
         <Route
           path="/challenge"
@@ -67,26 +100,6 @@ function App() {
             </Suspense>
           }
         />
-
-        {/* 잘못된 경로 */}
-        <Route element={<LayoutContainer />}>
-          <Route
-            path="*"
-            element={
-              <div className="flex flex-col justify-center items-center min-h-[calc(100vh-60px)] px-4 text-center">
-                <strong className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-2">
-                  404 Error
-                </strong>
-                <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-3">
-                  페이지를 찾을 수 없습니다.
-                </h1>
-                <p className="tossface text-sm sm:text-base md:text-lg text-gray-600 max-w-md">
-                  요청하신 페이지가 존재하지 않거나 이동되었을 수 있어요. 🙏
-                </p>
-              </div>
-            }
-          />
-        </Route>
       </Routes>
     </>
   );
