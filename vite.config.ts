f37a8c8df54aca54bf4ce6b4b62160a8c50a8adc
sonseongyeol <sonseong10@gmail.com>
2025-10-16 14:08:46 +0900

    Apply SPA 404 replace

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -6,6 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notFound: resolve(__dirname, "404.html"),
+      },
+    },
   },
   base: "/Codemate-Frontend/",
 });
