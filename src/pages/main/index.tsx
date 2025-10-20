4e523efa780a32b1965932e5221072767c3b02b7
sonseongyeol <sonseong10@gmail.com>
2025-10-20 09:52:21 +0900

    Add section banner in main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -70,6 +70,48 @@ function MainPage() {
       {/* 안내 섹션 */}
       <IntroBanner />
 
+      <section className="py-8 text-center">
+        <h2 className="text-2xl sm:text-3xl font-bold mb-10 text-gray-800">
+          더 이상 혼자 공부하지 말고, <br className="sm:hidden" />
+          알고리즘을 함께 재밌게 풀어봐요!
+        </h2>
+
+        <ul className="grid  sm:grid-cols-1 lg:grid-cols-3 gap-8 px-6 sm:px-10 max-w-6xl mx-auto">
+          <li className="flex flex-col items-center p-6 border border-red-200 bg-red-50 rounded-2xl shadow-sm">
+            <img
+              src="src/assets/contract.png"
+              alt="AI 평가 기능"
+              className="w-32 h-32 object-contain mb-4"
+            />
+            <p className="text-gray-700 font-medium">
+              AI가 제출 시간과 코드를 분석해 <br /> 합리적인 평가를 제공해요.
+            </p>
+          </li>
+
+          <li className="flex flex-col items-center p-6 border border-blue-200 bg-blue-50 rounded-2xl shadow-sm">
+            <img
+              src="src/assets/chat.png"
+              alt="채팅 협업 기능"
+              className="w-32 h-32 object-contain mb-4"
+            />
+            <p className="text-gray-700 font-medium">
+              실시간 채팅으로 함께 문제를 풀며 <br /> 더 쉽게 배우세요.
+            </p>
+          </li>
+
+          <li className="flex flex-col items-center p-6 border border-orange-200 bg-orange-50 rounded-2xl shadow-sm">
+            <img
+              src="src/assets/medal.png"
+              alt="랭킹 시스템"
+              className="w-32 h-32 object-contain mb-4"
+            />
+            <p className="text-gray-700 font-medium">
+              꾸준히 도전하고 성장해 <br /> 금메달을 획득하세요!
+            </p>
+          </li>
+        </ul>
+      </section>
+
       {/* 문제 선택 섹션 */}
       <ProblemList />
     </div>
